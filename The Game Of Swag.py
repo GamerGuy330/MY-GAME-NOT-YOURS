b7f61c8fae0824b08ddeda89b22e5128ca9b3b70
GamerGuy330 <metalfist@comcast.net>
2019-06-25 15:57:36 -0700

    I commit yeeeet

---
+++ The Game Of Swag.py
@@ -0,0 +1,44 @@
+import sys, pygame, random, pandas as pd
+from ship import Ship
+from asteroid import Asteroid
+from pygame.locals import *
+
+pygame.init()
+screen_info = pygame.display.Info()
+#set the width and height to the size of the screen
+size = (width, height) = (int(screen_info.current_w * 0.5), int(screen_info.current_h *))
+
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+color = (30, 0, 30)
+screen.fill(color)
+
+#read and store game data
+df = pd.read_csv("game_info.csv")
+
+#setup game variables
+asteroid = pygame.sprite.Group()
+numLevels = df["LevelNum"].max()
+level = df["LevelNum"].min()
+levelData = df.iloc[level]
+asteroidCount = levelData["AsteroidCount"]
+player = Ship((levelData["PlayerX"], levelData["PlayerY"]))
+
+def init():
+    global asteroidCount, asteroids, levelData
+    levelData = df.iloc[level]
+    player.reset((levelData["PlayerX"], levelData["PlayerY"]))
+    asteroids.empty()
+    asteroidCount = levelData["AsteroidCount"]
+    for i in range(asteroidCount):
+        asteroids.add(Asteroid((random.randint(50, width-50)), random.randint(15, 60)))
+
+def win():
+    font = pygame.font.SysFont(None, 70)
+    text = font.render("Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet", True, (255, 0, 0))
+    text_rect = text.get_rect()
+    text_rect.center = (width/2, height/2)
+    while True:
+        screen.fill(color)
+        screen.blit(text, text_rect)
+        pygame.display.flip()
