7a0921a794758536254a5153c131f247f7528fc6
cwksean10596 <x>
2019-06-26 03:42:18 -0700

    I commit yeeeeeeeeeet

--- ship.py
+++ ship.py
@@ -12,7 +12,12 @@ class Ship(pygame.sprite.Sprite):
         self.speed = pygame.math.Vector2(0, 0)
 
     def update(self):
+        screen_info = pygame.display.Info()
         self.rect.move_ip(self.speed)
+        if self.rect.left < 0 > screen_info.current_w:
+            self.speed[0] *= -1
+        if self.rect.top < 0 or self.rect.bottom > screen_info.current_h:
+            self.speed[1] *= -1
 
     def checkReset(self, endPos):
         return self.rect.center[0] > endPos
